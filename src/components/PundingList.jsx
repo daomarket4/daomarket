b54d73e7a4ba349e57bb9773918f0bd7ee3134a2
nanamingg <nana172@kakao.com>
2024-02-19 09:30:28 +0900

    ProgressbarList

--- src/components/PundingList.jsx
+++ src/components/PundingList.jsx
@@ -73,32 +73,41 @@ const PundingList = () => {
   return (
     <>
       <ProposalDataFetcher onDataFetched={handleDataFetched} />
-      <div className="container px-5 py-14 mx-auto grid grid-cols-3">
-        {proposals.map((proposal, index) => (
-          <div className="flex flex-wrap justify-center -m-4" key={index}>
-            <div className="lg:w-1/3 md:w-1/2 p-4 w-full">
-              <div className="block relative h-fit rounded overflow-hidden text-center">
-                <Link to={`/proposal/${index}`}>
-                  <img
-                    src={proposal.imageLink}
-                    alt="proposal.imageLink"
-                    className="mb-8 mr-2"
-                  />
-                </Link>
-              </div>
-              <div className="mt-4">
-                <h2 className="text-gray-900 title-font text-3xl font-medium text-center -mt-8">
-                  {proposal.title}
-                </h2>
-                <p className="mt-2 mb-8">
-                  {/* 진행상황 */}
-                  <ProgressbarList percentage={percentage.toFixed(2)} />맵 함수
-                  써야하나요..?
-                </p>
+      <div className="container px-5 py-14 mx-auto grid grid-cols-3 gap-4">
+        {proposals.map((proposal, index) => {
+          // 각 제안에 대한 진행율 계산
+          const amountRaisedInEther =
+            proposal && web3
+              ? web3.utils.fromWei(proposal.amountRaised, "ether")
+              : "0";
+          const fundingGoalInEther =
+            proposal && web3
+              ? web3.utils.fromWei(proposal.fundingGoal, "ether")
+              : "0";
+          const percentage =
+            (Number(amountRaisedInEther) / Number(fundingGoalInEther)) * 100;
+
+          return (
+            <div
+              key={index}
+              className="max-w-sm rounded overflow-hidden shadow-lg"
+            >
+              <Link to={`/proposal/${index}`}>
+                <img
+                  src={proposal.imageLink}
+                  alt="proposal"
+                  className="w-full"
+                />
+              </Link>
+              <div className="px-6 py-4">
+                <div className="font-bold text-xl mb-2">{proposal.title}</div>
+                <ProgressbarList
+                  percentage={isNaN(percentage) ? 0 : percentage.toFixed(2)}
+                />
               </div>
             </div>
-          </div>
-        ))}
+          );
+        })}
       </div>
     </>
   );
