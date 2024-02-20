b8b60c89144c1d434b11f25d0ec8c542c9e8327a
rootel <97437816+rootel123@users.noreply.github.com>
2024-02-20 16:16:49 +0900

    Update Main.jsx

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -7,7 +7,7 @@ import React from "react";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import ProgressbarMain from "./\bProgressbarMain.jsx";
+import ProgressbarMain from "./ProgressbarMain.jsx";
 import CountdownTimerMain from "./CountdownTimerMain.jsx";
 
 const Main = () => {
