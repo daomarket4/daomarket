77e481aba544a760eb6da742ee52342b9dfc6c47
JUNHYUK JUNG <kuyhnuj@outlook.com>
2024-02-23 15:20:56 +0900

    Edit Roles

--- src/pages/AboutUs.jsx
+++ src/pages/AboutUs.jsx
@@ -35,9 +35,10 @@ const AboutUs = () => {
                     <h2 className="title-font font-medium text-lg text-gray-900">
                       박준현
                     </h2>
-                    <h3 className="text-gray-500 mb-3">Project Team Leader</h3>
+                    <h3 className="text-gray-500 mb-3">프로젝트 팀장</h3>
                     <p className="mb-4">
-                      역할역역할역할역할역할역할 할말할말할말할말할말
+                      프로젝트 팀장으로서 프로젝트 전체적인 관리 및 솔루션을
+                      제시하였고, MultiSig 관련 코드 개발을 담당하였습니다.
                     </p>
                     <span className="inline-flex text-gray-500">
                       <a
@@ -54,37 +55,6 @@ const AboutUs = () => {
               <div className="p-4 lg:w-1/5 md:w-1/2">
                 <div className="h-full flex flex-col items-center text-center">
                   <img
-                    src={kihyunkim}
-                    alt="kihyunkim"
-                    className="mb-8 w-42 mr-2 rounded-full border border-black"
-                  />
-                  <div className="w-full">
-                    <h2 className="title-font font-medium text-lg text-gray-900">
-                      김기현
-                    </h2>
-                    <h3 className="text-gray-500 mb-3">Frontend</h3>
-                    <p className="mb-4">
-                      역할역역할역할역할역할역할 할말할말할말할말할말
-                    </p>
-                    <span className="inline-flex">
-                      <div className="text-gray-500">
-                        <div>
-                          <a
-                            href="https://github.com/rootel123"
-                            target="_blank"
-                            rel="noopener noreferrer"
-                          >
-                            <BsGithub className="text-xl text-black" />
-                          </a>
-                        </div>
-                      </div>
-                    </span>
-                  </div>
-                </div>
-              </div>
-              <div className="p-4 lg:w-1/5 md:w-1/2">
-                <div className="h-full flex flex-col items-center text-center">
-                  <img
                     src={junhyukjung}
                     alt="junhyukjung"
                     className="mb-8 w-42 mr-2 rounded-full border border-black"
@@ -93,9 +63,10 @@ const AboutUs = () => {
                     <h2 className="title-font font-medium text-lg text-gray-900">
                       정준혁
                     </h2>
-                    <h3 className="text-gray-500 mb-3">Frontend</h3>
+                    <h3 className="text-gray-500 mb-3">웹페이지 UI / UX</h3>
                     <p className="mb-4">
-                      역할역역할역할역할역할역할 할말할말할말할말할말
+                      프로젝트 웹페이지의 디자인과 기능 구현을 담당하였으며, UI
+                      / UX 개선을 하였습니다.
                     </p>
                     <span className="inline-flex">
                       <div className="text-gray-500">
@@ -116,23 +87,27 @@ const AboutUs = () => {
               <div className="p-4 lg:w-1/5 md:w-1/2">
                 <div className="h-full flex flex-col items-center text-center">
                   <img
-                    src={juyoungjung}
-                    alt="juyoungjung"
+                    src={kihyunkim}
+                    alt="kihyunkim"
                     className="mb-8 w-42 mr-2 rounded-full border border-black"
                   />
                   <div className="w-full">
                     <h2 className="title-font font-medium text-lg text-gray-900">
-                      정주영
+                      김기현
                     </h2>
-                    <h3 className="text-gray-500 mb-3">Backend</h3>
+                    <h3 className="text-gray-500 mb-3">
+                      스마트 컨트랙트 & 웹페이지 연동
+                    </h3>
                     <p className="mb-4">
-                      역할역역할역할역할역할역할 할말할말할말할말할말
+                      웹페이지에서 스마트 컨트랙트가 원할이 구현될 수 있도록
+                      기능을 구현하였으며, 웹페이지 개발에도 보조로
+                      참여하였습니다.
                     </p>
                     <span className="inline-flex">
                       <div className="text-gray-500">
                         <div>
                           <a
-                            href="https://github.com/wndud5814"
+                            href="https://github.com/rootel123"
                             target="_blank"
                             rel="noopener noreferrer"
                           >
@@ -155,15 +130,49 @@ const AboutUs = () => {
                     <h2 className="title-font font-medium text-lg text-gray-900">
                       김정빈
                     </h2>
-                    <h3 className="text-gray-500 mb-3">Backend</h3>
+                    <h3 className="text-gray-500 mb-3">스마트 컨트랙트 개발</h3>
                     <p className="mb-4">
-                      역할역역할역할역할역할역할 할말할말할말할말할말
+                      프로젝트 스마트 컨트랙트의 전체적인 부분을 개발하였으며,
+                      웹페이지에서 스마트 컨트랙트와 연동될 수 있도록
+                      보조하였습니다.
                     </p>
                     <span className="inline-flex">
                       <div className="text-gray-500">
                         <div>
                           <a
                             href="https://github.com/nanamingg"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                          >
+                            <BsGithub className="text-xl text-black" />
+                          </a>
+                        </div>
+                      </div>
+                    </span>
+                  </div>
+                </div>
+              </div>
+              <div className="p-4 lg:w-1/5 md:w-1/2">
+                <div className="h-full flex flex-col items-center text-center">
+                  <img
+                    src={juyoungjung}
+                    alt="juyoungjung"
+                    className="mb-8 w-42 mr-2 rounded-full border border-black"
+                  />
+                  <div className="w-full">
+                    <h2 className="title-font font-medium text-lg text-gray-900">
+                      정주영
+                    </h2>
+                    <h3 className="text-gray-500 mb-3">스마트 컨트랙트 개발</h3>
+                    <p className="mb-4">
+                      스마트 컨트랙트 개발에 보조로 참여했으며, dao와 관련하여
+                      코드 개발을 담당하였습니다.
+                    </p>
+                    <span className="inline-flex">
+                      <div className="text-gray-500">
+                        <div>
+                          <a
+                            href="https://github.com/wndud5814"
                             target="_blank"
                             rel="noopener noreferrer"
                           >
