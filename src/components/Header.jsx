abe41e8446de731dbb9681e9a0b0c48d95e37602
JUNHYUK JUNG <kuyhnuj@outlook.com>
2024-02-28 09:30:20 +0900

    Changed LOGO

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -72,7 +72,7 @@ const Header = ({ profileImage }) => {
         <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
           <Link to="/">
             <div className="flex mt-1 title-font font-medium items-center text-gray-900 mb-4 md:mb-0 dark:text-white">
-              <img src={DAOM} alt="DAOM" className="mr-2 w-32 pb-4" />
+              <img src={DAOM} alt="DAOM" className="mr-4 w-28 pb-2" />
             </div>
           </Link>
           <div className="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-400	flex flex-wrap items-center text-base justify-center">
